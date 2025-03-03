585a05bcd1c212308b1af6f109ff5e1d21526507
Angelica Le <angelica.le@techeducators.co.uk>
2025-03-03 08:38:53 +0000

    fixed view card

--- src/components/GoogleReview.jsx
+++ src/components/GoogleReview.jsx
@@ -1,16 +1,22 @@
 "use client";
+import Image from "next/image";
 import React from "react";
 
 export default function GoogleReview() {
   return (
-    <div className="pt-8">
+    <div
+      className="w-full bg-left-top bg-no-repeat justify-center flex bg-cover items-center relative pt-8"
+      style={{
+        backgroundImage: "url(/review-background.jpg)",
+      }}
+    >
       {/* <!-- Elfsight Google Reviews | Untitled Google Reviews --> */}
       <script
         src="https://static.elfsight.com/platform/platform.js"
         async
       ></script>
       <div
-        class="elfsight-app-6b7f1c4d-9ca8-4469-9d65-7232a5d1686b"
+        className="elfsight-app-6b7f1c4d-9ca8-4469-9d65-7232a5d1686b"
         data-elfsight-app-lazy
       ></div>
     </div>
