a71a325592b9aaa4a61605f06554534c041bac31
Angelicale <gianghgle@gmail.com>
2025-09-29 14:17:25 +0100

    updated form in typescript

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -11,8 +11,8 @@ export default function Map() {
           height="100%"
           frameBorder="0"
           title="map"
-          marginHeight="0"
-          marginWidth="0"
+          // marginHeight="0"
+          // marginWidth="0"
           scrolling="no"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2359.225100455157!2d-0.3961967232084937!3d53.749874772409136!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4878bfa8246adea3%3A0xb6e323f914fa75b7!2zUGjhu58gTGFu!5e0!3m2!1sen!2suk!4v1739887767357!5m2!1sen!2suk"
         ></iframe>
