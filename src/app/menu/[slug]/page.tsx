e151b674f90b2dca7efc5b75c0e5f61191f66327
Angelicale <gianghgle@gmail.com>
2025-09-29 14:08:29 +0100

    migrate to Typescript

--- src/app/menu/[slug]/page.tsx
+++ src/app/menu/[slug]/page.tsx
@@ -1,52 +1,61 @@
 "use client";
 
+import { dishes, findByCategory } from "@/libs/dishes";
 import DelayedImage from "@/components/DelayedImage";
 import MenuNav from "@/components/MenuNav";
-import { dishes } from "@/dishes/dishes";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import Image from "next/image";
-import React, { useState } from "react";
-import { Suspense } from "react";
+import React, { Suspense, SyntheticEvent, useState } from "react";
 
-export default function StarterPage() {
-  const starterDishes = dishes.filter((starterdish) => {
-    if (starterdish.category === "starter") return starterdish;
-  });
-  // console.log("starterDishes", starterDishes);
+export default function CategoryPage({
+  //! "params" used to be plain objects, but with React 19 - Next treats them as async values
+  //! "params" is now a Promise
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = React.use(params); // <-- unwrap the Promise
+  const items = findByCategory(slug, dishes);
+
+  console.log("items", items);
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  function handleClickImage(index) {
+  function handleClickImage(index: number) {
     setCurrentIndex(index);
   }
 
-  const changePage = (direction) => {
+  const changePage = (direction: string) => {
     setCurrentIndex((prevIndex) => {
       if (direction === "next") {
-        return prevIndex === starterDishes.length - 1 ? 0 : prevIndex + 1;
+        return prevIndex === items.length - 1 ? 0 : prevIndex + 1;
       } else {
-        return prevIndex === 0 ? starterDishes.length - 1 : prevIndex - 1;
+        return prevIndex === 0 ? items.length - 1 : prevIndex - 1;
       }
     });
   };
 
+  const bgImage =
+    slug === "drink"
+      ? "url(/drink-background.jpg)"
+      : "url(/menu-background02.png)";
+
   return (
     <div className="relative">
       <MenuNav />
-
       <div className=" flex justify-center items-center flex-col p-4 w-full">
         <div
-          className="absolute inset-0 opacity-25 z-0 bg-center bg-no-repeat bg-cover bg-fixed"
+          className="absolute inset-0 opacity-25 z-0 bg-top bg-no-repeat bg-cover bg-fixed"
           style={{
-            backgroundImage: "url(/menu-background02.png)",
+            backgroundImage: bgImage,
           }}
         ></div>
         <div className="hidden">
-          {starterDishes.map((dish, index) => (
+          {items.map((dish, index) => (
             <div key={dish.id}>
               <Image
                 src={dish.src}
-                alt="img"
+                alt={`${slug} ${dish.id}`}
                 width={100}
                 height={100}
                 onClick={() => handleClickImage(index)}
@@ -66,13 +75,13 @@ export default function StarterPage() {
           <div className=" shadow-2xl animate-fadeInDown pt-4 w-[80vw] md:w-[60vw] lg:w-[40vw]">
             <Suspense>
               <DelayedImage
-                src={starterDishes[currentIndex].src}
-                alt="starter menu"
-                width={0}
-                height={0}
+                src={items[currentIndex]?.src}
+                alt={`${items[currentIndex].id}`}
+                width={800}
+                height={800}
                 sizes="100%"
                 className="transition-opacity duration-500 opacity-0"
-                onLoad={(e) => {
+                onLoad={(e: SyntheticEvent<HTMLImageElement>) => {
                   e.currentTarget.classList.remove("opacity-0");
                   e.currentTarget.classList.add("opacity-100");
                 }}
@@ -83,7 +92,7 @@ export default function StarterPage() {
               />
             </Suspense>
           </div>
-          {currentIndex < starterDishes.length - 1 && (
+          {currentIndex < items.length - 1 && (
             <ChevronRight
               onClick={() => changePage("next")}
               className="fixed top-1/2 -translate-y-1/2 right-[2vw] md:right-[15vw] w-8 h-8 md:w-10 md:h-10 border rounded-2xl border-[--darktext]"
